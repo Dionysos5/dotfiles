b6261aeb67ed0dd03ce4df051fa04035c34332a8
Denis Bakhoum <denis.bakhoum@harvy.tech>
2024-05-10 12:04:33 +0200

    update brew script for cron task

--- brew_update.sh
+++ brew_update.sh
@@ -1,4 +1,12 @@
-#!/bin/bash -l
-brew update
-brew upgrade
+#!/bin/zsh 
+
+LOG_FILE="$HOME/.logs/homebrew.log"
+
+date &>> "$LOG_FILE"
+
+/opt/homebrew/bin/brew update &>> "$LOG_FILE"
+
+/opt/homebrew/bin/brew upgrade &>> "$LOG_FILE"
+
+echo "-----------------------------------" >> "$LOG_FILE"
 
